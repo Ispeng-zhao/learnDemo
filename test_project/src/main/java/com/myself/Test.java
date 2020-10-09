74c918388893feb2a8762bdb0193667f07e19753
peng <1041059452@qq.com>
2020-10-09 20:25:42 +0800

    1

---
+++ test_project/src/main/java/com/myself/Test.java
@@ -0,0 +1,12 @@
+package com.myself;
+
+/**
+ * @Author peng
+ * @Date 2020/10/9 20:24
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println( " Git test ! ");
+    }
+}
